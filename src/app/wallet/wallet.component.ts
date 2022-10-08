e487d2bf74789c928d58c79ab0b2031b87993d17
Angeles Cerdosino <angelescerdosi@gmail.com>
2022-10-08 20:53:19 -0300

    adding wallet component info

--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CardDataService } from '../services/card-data-service.service';
 
 @Component({
   selector: 'app-wallet',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalletComponent implements OnInit {
 
-  constructor() { }
+  constructor(public cardData: CardDataService) { }
 
   ngOnInit(): void {
   }
+
+  placeBet(){
+    if(this.cardData.bet == null){
+      return;
+    }
+    if(this.cardData.wallet < this.cardData.bet){
+      return;
+    }
+    this.cardData.gameActive = true;
+    this.cardData.getRandomCard('user');
+    this.cardData.getRandomCard('dealer');
+    this.cardData.getRandomCard('user');
+    this.cardData.playAudio("./assets/audio/198234__everythingsounds__credit-card-swipe.wav");
+    //this.cardData.wallet -= this.cardData.bet;
+    if(this.cardData.userScore > 21){
+      this.cardData.gameResult = "Derrotado";
+      
+      this.cardData.reset();
+    }
+  }
+
+  validateInput(event){
+    if (event.target.validity.valid){
+      if(event.target.value > this.cardData.wallet){
+        event.target.value = '';
+      }
+      if(event.target.value === ''){
+        event.target.value = '';
+      }
+      
+    }
+    else{
+      event.target.value = '';
+    }
+
+
+}
 
 }
