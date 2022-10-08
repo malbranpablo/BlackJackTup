676c16cc52366b14a7eb2d2e43cafbf90441a0dd
Pablo Malbran <pablo.malbran@mercadolibre.com>
2022-10-08 16:54:10 -0300

    Reorder componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './main-area/card/card.component';
+import { MainAreaComponent } from './main-area/main-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    MainAreaComponent
   ],
   imports: [
     BrowserModule,
