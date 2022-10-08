721781614de2786d6aa3e583d44f6fe533c69676
Pablo Malbran <pablo.malbran@mercadolibre.com>
2022-10-08 16:54:28 -0300

    Reorder componets

---
+++ src/app/main-area/main-area.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MainAreaComponent } from './main-area.component';
+
+describe('MainAreaComponent', () => {
+  let component: MainAreaComponent;
+  let fixture: ComponentFixture<MainAreaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MainAreaComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(MainAreaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
