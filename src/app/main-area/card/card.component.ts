a0f48e47bf694502d51f5b0018975e882851334d
Angeles Cerdosino <angelescerdosi@gmail.com>
2022-10-08 20:59:12 -0300

    updating card and main area components

--- src/app/main-area/card/card.component.ts
+++ src/app/main-area/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input()
+  cardNumber:string;
+
+  @Input('cardURL')
+  cardURL:string;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
+
 
 }
--- src/app/main-area/main-area.component.ts
+++ src/app/main-area/main-area.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CardDataService } from '../services/card-data-service.service';
 
 @Component({
   selector: 'app-main-area',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainAreaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public cardData: CardDataService) { }
 
   ngOnInit(): void {
   }
